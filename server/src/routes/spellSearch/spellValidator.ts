47a609039675de9a3783a20ba94eddb38e3cfc35
Brendon Hudnell <bthudnell@gmail.com>
2021-03-14 17:43:56 -0600

    added type coercion to requestValidator and implemented it in spellValidator. Updated API doc to reflect the above change

--- server/src/routes/spellSearch/spellValidator.ts
+++ server/src/routes/spellSearch/spellValidator.ts
@@ -1,177 +1,139 @@
 import { requestValidator } from '../../validator';
 
-export const spellSearchValidator = requestValidator({
-	query: {
-		type: 'object',
-		additionalProperties: false,
-		properties: {
-			searchString: {
-				type: 'string',
-			},
-			spellResistance: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			alchemist: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			antipaladin: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			arcanist: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			bard: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			bloodrager: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			cleric: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			druid: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			hunter: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			inquisitor: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			investigator: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			magus: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			medium: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			mesmerist: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			occultist: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			oracle: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			paladin: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			psychic: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			ranger: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			shaman: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			skald: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			sorcerer: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			spiritualist: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			summoner: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			summoner_unchained: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			witch: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			wizard: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			fortitude: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			reflex: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			will: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			none: {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			'0th': {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			'1st': {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			'2nd': {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			'3rd': {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			'4th': {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			'5th': {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			'6th': {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			'7th': {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			'8th': {
-				type: 'string',
-				enum: ['true', 'false'],
-			},
-			'9th': {
-				type: 'string',
-				enum: ['true', 'false'],
+export const spellSearchValidator = requestValidator(
+	{
+		query: {
+			type: 'object',
+			additionalProperties: false,
+			properties: {
+				searchString: {
+					type: 'string',
+				},
+				spellResistance: {
+					type: 'boolean',
+				},
+				alchemist: {
+					type: 'boolean',
+				},
+				antipaladin: {
+					type: 'boolean',
+				},
+				arcanist: {
+					type: 'boolean',
+				},
+				bard: {
+					type: 'boolean',
+				},
+				bloodrager: {
+					type: 'boolean',
+				},
+				cleric: {
+					type: 'boolean',
+				},
+				druid: {
+					type: 'boolean',
+				},
+				hunter: {
+					type: 'boolean',
+				},
+				inquisitor: {
+					type: 'boolean',
+				},
+				investigator: {
+					type: 'boolean',
+				},
+				magus: {
+					type: 'boolean',
+				},
+				medium: {
+					type: 'boolean',
+				},
+				mesmerist: {
+					type: 'boolean',
+				},
+				occultist: {
+					type: 'boolean',
+				},
+				oracle: {
+					type: 'boolean',
+				},
+				paladin: {
+					type: 'boolean',
+				},
+				psychic: {
+					type: 'boolean',
+				},
+				ranger: {
+					type: 'boolean',
+				},
+				shaman: {
+					type: 'boolean',
+				},
+				skald: {
+					type: 'boolean',
+				},
+				sorcerer: {
+					type: 'boolean',
+				},
+				spiritualist: {
+					type: 'boolean',
+				},
+				summoner: {
+					type: 'boolean',
+				},
+				summoner_unchained: {
+					type: 'boolean',
+				},
+				witch: {
+					type: 'boolean',
+				},
+				wizard: {
+					type: 'boolean',
+				},
+				fortitude: {
+					type: 'boolean',
+				},
+				reflex: {
+					type: 'boolean',
+				},
+				will: {
+					type: 'boolean',
+				},
+				none: {
+					type: 'boolean',
+				},
+				'0th': {
+					type: 'boolean',
+				},
+				'1st': {
+					type: 'boolean',
+				},
+				'2nd': {
+					type: 'boolean',
+				},
+				'3rd': {
+					type: 'boolean',
+				},
+				'4th': {
+					type: 'boolean',
+				},
+				'5th': {
+					type: 'boolean',
+				},
+				'6th': {
+					type: 'boolean',
+				},
+				'7th': {
+					type: 'boolean',
+				},
+				'8th': {
+					type: 'boolean',
+				},
+				'9th': {
+					type: 'boolean',
+				},
 			},
 		},
 	},
-});
+	true
+);
