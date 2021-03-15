693c08a6b67fd66d3314f4848208c803213df8ae
Brendon Hudnell <bthudnell@gmail.com>
2021-03-14 18:07:00 -0600

    added utils tests

---
+++ server/test/utils/isProcessObjectEmpty.test.ts
@@ -0,0 +1,32 @@
+import { ProcessObject } from '../../src/routes/spellSearch';
+import { isProcessObjectEmpty } from '../../src/utils';
+
+describe('isProcessObjectEmpty', () => {
+	it('should return true for an empty object', () => {
+		const object: ProcessObject = {
+			searchString: '',
+			spellResistance: '',
+			classes: [],
+			saves: [],
+			spellLevels: [],
+		};
+
+		const isEmpty = isProcessObjectEmpty(object);
+
+		expect(isEmpty).toBe(true);
+	});
+
+	it('should return false for a non-empty object', () => {
+		const object: ProcessObject = {
+			searchString: 'not',
+			spellResistance: 'empty',
+			classes: ['still'],
+			saves: ['not empty'],
+			spellLevels: [123],
+		};
+
+		const isEmpty = isProcessObjectEmpty(object);
+
+		expect(isEmpty).toBe(false);
+	});
+});
