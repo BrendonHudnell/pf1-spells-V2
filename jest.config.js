85defabf6ca973be341a674577c140dc85ef7c13
Brendon Hudnell <bthudnell@gmail.com>
2021-03-17 09:48:31 -0600

    added minimum coverage threshold for tests

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
 	testEnvironment: 'node',
 	roots: ['<rootDir>/server/test'],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
 };
